5810984357be231bd2bc977f4ffb64e761d09a19
ctrimble98 <connor@trimbles.co.uk>
2019-04-21 02:09:57 +0100

    Most of cnf parsing done

--- src/parser.cpp
+++ src/parser.cpp
@@ -6,9 +6,109 @@ void parseCNF() {
 
     //Read line until start with p -> read chars no whitespace or \n until 0 for each clause
 
-    char c;
-    while (std::cin >> c) {
+    std::string headerLine, token;
+    bool isPreamble = true;
+    bool comments = true;
+    char lineType;
+    char delim = ' ';
 
-        std::cout << c;
+    int variables, clauses, iter;
+
+    std::stringstream iss;
+
+    while (isPreamble && std::getline(std::cin, headerLine)) {
+
+        if (headerLine.length() > 1)  {
+            lineType = headerLine[0];
+            std::cout << lineType << std::endl;
+            switch (lineType) {
+                case 'c':
+                    break;
+                case 'p':
+
+                    iter = 0;
+                    iss << headerLine;
+                    while (std::getline(iss, token, delim)) {
+
+                        std::cout << "Problem token: |" << token << "|" << std::endl;
+
+                        switch (iter) {
+
+                            case 0:
+
+                                if (token != "p") {
+                                    returnProgram(1);
+                                }
+                                break;
+                            case 1:
+
+                                if (token != "cnf") {
+                                    returnProgram(1);
+                                }
+                                break;
+                            case 2:
+
+                                try {
+
+                                    variables = std::stoi(token);
+                                } catch (std::invalid_argument) {
+
+                                    std::cout << "variables failed: " << token << std::endl;
+                                    returnProgram(1);
+                                }
+                                break;
+                            case 3:
+
+                                try {
+
+                                    clauses = std::stoi(token);
+                                } catch (std::invalid_argument) {
+
+                                    std::cout << "clauses failed: " << token << std::endl;
+                                    returnProgram(1);
+                                }
+                                break;
+                            default:
+                                returnProgram(1);
+                        }
+                        iter++;
+                    }
+                    isPreamble = false;
+                    break;
+
+                default:
+                    returnProgram(1);
+            }
+        }
     }
+    std::cout << clauses << " " << variables << std::endl;
+
+    std::vector<std::vector<int>> clauseArray(clauses);
+    std::vector<int> currentClause(10);
+    int i, var;
+    i = 0;
+    while (std::getline(std::cin, token, delim)) {
+
+        try {
+
+            var = std::stoi(token);
+            if (var > variables || (var*-1) > variables) {
+                returnProgram(1);
+            }
+            currentClause.at(i) = var;
+
+            std::cout << "Clause var: " << var << std::endl;
+            i++;
+        } catch (std::invalid_argument) {
+
+            std::cout << "Parsing clauses failed on: " << token << std::endl;
+            returnProgram(1);
+        }
+    }
+
+    // char c;
+    // while (std::cin >> c) {
+    //
+    //     std::cout << c;
+    // }
 }
--- src/reductions.h
+++ src/reductions.h
@@ -1,10 +1,13 @@
 #ifndef REDUCTIONS_H
 #define REDUCTIONS_H
 
+#include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
+#include <stdexcept>
+#include <vector>
 
 void returnProgram(int returnVal);
 
